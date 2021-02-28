dec4ffef47a396e958a9c11ddaa2ed0d76e17a7e
victoriafolyush <victoriafolyush@gmail.com>
2021-02-28 14:17:46 +0200

    refactored

--- quickSort.go
+++ quickSort.go
@@ -2,49 +2,49 @@ package main
 
 import "fmt"
 
-func getPivot (array [] int) int {
+func getPivot(array []int) int {
 	res := array[0]
 	return res
 }
 
-func swapPivot (array [] int, start int, end int) int {
+func swapPivot(array []int, start int, end int) int {
 	pivot := getPivot(array)
 	pivot = array[start]
 	swapIdx := start
-	for i := start + 1; i <= end; i++{
+	for i := start + 1; i <= end; i++ {
 		if pivot >= array[i] {
 			swapIdx++
-			temp := array [i]
-			array [i] = array[swapIdx]
+			temp := array[i]
+			array[i] = array[swapIdx]
 			array[swapIdx] = temp
 		}
 	}
-	temp := array [start]
-	array [start] = array[swapIdx]
+	temp := array[start]
+	array[start] = array[swapIdx]
 	array[swapIdx] = temp
 
 	return swapIdx
 }
 
-func sorting (array [] int, left int, right int) []int {
+func sorting(array []int, left int, right int) []int {
 	if left < right {
 		pivotIndex := swapPivot(array, left, right)
-		sorting(array, left, pivotIndex - 1)
-		sorting(array, pivotIndex + 1, right)
+		sorting(array, left, pivotIndex-1)
+		sorting(array, pivotIndex+1, right)
 	}
 	return array
 }
 
-func quickSort (array [] int, getPivot func([] int) int) [] int {
-	sorting (array, 0, len (array) - 1)
-	if len(array) > 0{
+func quickSort(array []int, getPivot func([]int) int) []int {
+	sorting(array, 0, len(array)-1)
+	if len(array) > 0 {
 		getPivot(array)
 	}
 	return array
 }
 
 func main() {
-	nums := []int {82, 252, 101, 83}
+	nums := []int{82, 252, 101, 83}
 	fmt.Println(nums)
 	fmt.Println(quickSort(nums, getPivot))
 }
